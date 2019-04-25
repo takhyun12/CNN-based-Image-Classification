2c58e56a10f683f5834013ea17187a354714c15c
takhyun12 <41291493+takhyun12@users.noreply.github.com>
2019-04-25 11:21:39 +0900

    Add files via upload

---
+++ CNN_UI/CNN_UI/Main.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace CNN_UI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.img_search_btn = new System.Windows.Forms.Button();
+            this.img_download_btn = new System.Windows.Forms.Button();
+            this.image_lvw = new System.Windows.Forms.ListView();
+            this.img_pbx = new System.Windows.Forms.PictureBox();
+            this.result_tbx = new System.Windows.Forms.TextBox();
+            this.info_lbl = new System.Windows.Forms.Label();
+            this.image_list = new System.Windows.Forms.ImageList(this.components);
+            this.inputGroup = new System.Windows.Forms.GroupBox();
+            this.outputGroup = new System.Windows.Forms.GroupBox();
+            this.loading_pbx = new System.Windows.Forms.PictureBox();
+            this.close_btn = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img_pbx)).BeginInit();
+            this.inputGroup.SuspendLayout();
+            this.outputGroup.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.loading_pbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close_btn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img_search_btn
+            // 
+            this.img_search_btn.Font = new System.Drawing.Font("나눔고딕", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.img_search_btn.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.img_search_btn.Location = new System.Drawing.Point(21, 393);
+            this.img_search_btn.Name = "img_search_btn";
+            this.img_search_btn.Size = new System.Drawing.Size(206, 42);
+            this.img_search_btn.TabIndex = 1;
+            this.img_search_btn.Text = "이미지 분석";
+            this.img_search_btn.UseVisualStyleBackColor = true;
+            this.img_search_btn.Click += new System.EventHandler(this.img_search_btn_Click);
+            // 
+            // img_download_btn
+            // 
+            this.img_download_btn.Location = new System.Drawing.Point(233, 364);
+            this.img_download_btn.Name = "img_download_btn";
+            this.img_download_btn.Size = new System.Drawing.Size(184, 71);
+            this.img_download_btn.TabIndex = 2;
+            this.img_download_btn.Text = "이미지 다운로드";
+            this.img_download_btn.UseVisualStyleBackColor = true;
+            this.img_download_btn.Click += new System.EventHandler(this.img_download_btn_Click);
+            // 
+            // image_lvw
+            // 
+            this.image_lvw.Location = new System.Drawing.Point(24, 25);
+            this.image_lvw.Name = "image_lvw";
+            this.image_lvw.Size = new System.Drawing.Size(635, 409);
+            this.image_lvw.TabIndex = 3;
+            this.image_lvw.UseCompatibleStateImageBehavior = false;
+            // 
+            // img_pbx
+            // 
+            this.img_pbx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.img_pbx.Location = new System.Drawing.Point(21, 26);
+            this.img_pbx.Name = "img_pbx";
+            this.img_pbx.Size = new System.Drawing.Size(396, 327);
+            this.img_pbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_pbx.TabIndex = 4;
+            this.img_pbx.TabStop = false;
+            this.img_pbx.DragDrop += new System.Windows.Forms.DragEventHandler(this.img_pbx_DragDrop);
+            this.img_pbx.DragEnter += new System.Windows.Forms.DragEventHandler(this.img_pbx_DragEnter);
+            // 
+            // result_tbx
+            // 
+            this.result_tbx.Font = new System.Drawing.Font("나눔고딕", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.result_tbx.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.result_tbx.Location = new System.Drawing.Point(21, 364);
+            this.result_tbx.Name = "result_tbx";
+            this.result_tbx.ReadOnly = true;
+            this.result_tbx.Size = new System.Drawing.Size(206, 22);
+            this.result_tbx.TabIndex = 5;
+            this.result_tbx.Text = "미동작 (0%)";
+            this.result_tbx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // info_lbl
+            // 
+            this.info_lbl.AutoSize = true;
+            this.info_lbl.Font = new System.Drawing.Font("나눔고딕", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.info_lbl.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.info_lbl.Location = new System.Drawing.Point(93, 173);
+            this.info_lbl.Name = "info_lbl";
+            this.info_lbl.Size = new System.Drawing.Size(267, 16);
+            this.info_lbl.TabIndex = 6;
+            this.info_lbl.Text = "이미지를 이곳으로 드래그하여 가져오세요 :)";
+            // 
+            // image_list
+            // 
+            this.image_list.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.image_list.ImageSize = new System.Drawing.Size(16, 16);
+            this.image_list.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // inputGroup
+            // 
+            this.inputGroup.BackColor = System.Drawing.Color.Transparent;
+            this.inputGroup.Controls.Add(this.loading_pbx);
+            this.inputGroup.Controls.Add(this.info_lbl);
+            this.inputGroup.Controls.Add(this.result_tbx);
+            this.inputGroup.Controls.Add(this.img_download_btn);
+            this.inputGroup.Controls.Add(this.img_pbx);
+            this.inputGroup.Controls.Add(this.img_search_btn);
+            this.inputGroup.Font = new System.Drawing.Font("나눔고딕", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.inputGroup.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.inputGroup.Location = new System.Drawing.Point(25, 51);
+            this.inputGroup.Name = "inputGroup";
+            this.inputGroup.Size = new System.Drawing.Size(444, 453);
+            this.inputGroup.TabIndex = 61;
+            this.inputGroup.TabStop = false;
+            this.inputGroup.Text = "머신러닝 기반 이미지 검색";
+            // 
+            // outputGroup
+            // 
+            this.outputGroup.BackColor = System.Drawing.Color.Transparent;
+            this.outputGroup.Controls.Add(this.image_lvw);
+            this.outputGroup.Font = new System.Drawing.Font("나눔고딕", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.outputGroup.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.outputGroup.Location = new System.Drawing.Point(487, 52);
+            this.outputGroup.Name = "outputGroup";
+            this.outputGroup.Size = new System.Drawing.Size(682, 453);
+            this.outputGroup.TabIndex = 62;
+            this.outputGroup.TabStop = false;
+            this.outputGroup.Text = "이미지 수집현황";
+            // 
+            // loading_pbx
+            // 
+            this.loading_pbx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loading_pbx.Image = global::CNN_UI.Properties.Resources.loading;
+            this.loading_pbx.Location = new System.Drawing.Point(155, 138);
+            this.loading_pbx.Name = "loading_pbx";
+            this.loading_pbx.Size = new System.Drawing.Size(122, 100);
+            this.loading_pbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.loading_pbx.TabIndex = 7;
+            this.loading_pbx.TabStop = false;
+            this.loading_pbx.Visible = false;
+            // 
+            // close_btn
+            // 
+            this.close_btn.Image = global::CNN_UI.Properties.Resources.close;
+            this.close_btn.Location = new System.Drawing.Point(1169, 9);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(16, 16);
+            this.close_btn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.close_btn.TabIndex = 8;
+            this.close_btn.TabStop = false;
+            this.close_btn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.close_btn_MouseClick);
+            // 
+            // Main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::CNN_UI.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(1194, 528);
+            this.Controls.Add(this.close_btn);
+            this.Controls.Add(this.outputGroup);
+            this.Controls.Add(this.inputGroup);
+            this.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "이미지 인식 및 검색 프로그램";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Main_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.img_pbx)).EndInit();
+            this.inputGroup.ResumeLayout(false);
+            this.inputGroup.PerformLayout();
+            this.outputGroup.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.loading_pbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close_btn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button img_search_btn;
+        private System.Windows.Forms.Button img_download_btn;
+        private System.Windows.Forms.ListView image_lvw;
+        private System.Windows.Forms.PictureBox img_pbx;
+        private System.Windows.Forms.TextBox result_tbx;
+        private System.Windows.Forms.Label info_lbl;
+        private System.Windows.Forms.ImageList image_list;
+        private System.Windows.Forms.GroupBox inputGroup;
+        private System.Windows.Forms.GroupBox outputGroup;
+        private System.Windows.Forms.PictureBox loading_pbx;
+        private System.Windows.Forms.PictureBox close_btn;
+    }
+}
+
---
+++ CNN_UI/CNN_UI/Main.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace CNN_UI
+{
+    public partial class Main : Form
+    {
+        // 폼의 드래그 이동에 활용될 전역변수
+        int TogMove;
+        int MvalX;
+        int MvalY;
+
+        public Main()
+        {
+            InitializeComponent();
+            this.img_pbx.AllowDrop = true; // 픽쳐 박스의 드래그 앤 드롭 기능 활성화
+
+            try
+            {
+                // 이미지 다운로드 기능의 재사용을 위해 폴더 삭제
+                string image_path = @"C:\C#\NP\downloads";
+                string[] temp_files = Directory.GetFiles(image_path, "*", SearchOption.AllDirectories);
+                foreach (string file in temp_files)
+                {
+                    File.Delete(file);
+                }
+                Directory.Delete(image_path, true);
+            }
+            catch { }
+        }
+
+        private void img_pbx_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop); // 드래그 앤 드롭으로 가져온 파일들을 배열에 저장
+            string file_path = files[0]; // 하나의 파일만 검사할 것이므로, 배열의 첫번째 파일의 경로를 가져옴
+            string file_ext = Path.GetExtension(file_path); // 확장자만 추출함 ex) .jpg or .jpeg 
+            if (file_ext == ".jpeg" || file_ext == ".jpg") // 이미지 확장자인 경우에만 계속 진행
+            {
+                try
+                {
+                    // 재사용을 위한 초기화
+                    image_lvw.Items.Clear();
+                    while (image_lvw.Items.Count > 0)
+                    {
+                        var item = image_lvw.Items[image_lvw.Items.Count - 1];
+                        image_lvw.Items.RemoveAt(image_lvw.Items.Count - 1);
+                        if (item is IDisposable)
+                        {
+                            ((IDisposable)item).Dispose();
+                        }
+                    }
+                    image_list.Images.Clear();
+                    image_list.Dispose();
+                }
+                catch { }
+
+                try
+                {
+                    string image_path = @"C:\C#\NP\downloads";
+                    string[] temp_files = Directory.GetFiles(image_path, "*", SearchOption.AllDirectories);
+                    foreach (string file in temp_files)
+                    {
+                        File.Delete(file);
+                    }
+                    Directory.Delete(image_path, true);
+                }
+                catch{}
+
+                img_pbx.Load(file_path); // 사용자가 업로드한 이미지를 화면(픽쳐 박스)에 표시
+                info_lbl.Visible = false; // 드래그 앤 드롭을 유도하는 라벨을 비활성화하여 안보이도록 함  
+            }
+            else MessageBox.Show("jpg 형식의 이미지만 업로드 가능합니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        }
+
+        private void img_pbx_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private bool Image_Searching()
+        {
+            CheckForIllegalCrossThreadCalls = false; // 크로스 쓰레드 문제점 해결을 위한 예외처리
+
+            string file_path = img_pbx.ImageLocation; // 이미지 경로를 가져옴
+            string file_name = Path.GetFileName(file_path); // 파일명만 추출
+
+            string temp_image_path = @"C:\C#\NP\image";
+            string temp_file_path = temp_image_path + @"\" + file_name;
+
+            // 폴더가 존재하지 않는다면, 생성
+            if (!Directory.Exists(temp_image_path))
+                Directory.CreateDirectory(temp_image_path);
+            else
+            {
+                // 재사용을 위해 임시 이미지를 깔끔하게 삭제함
+                string[] files = Directory.GetFiles(temp_image_path);
+                foreach (string file in files)
+                {
+                    string f_name = Path.GetFileName(file);
+                    string f_path = temp_image_path + @"\" + f_name;
+                    File.Delete(f_path);
+                }
+
+            }
+
+            // 파일을 임시폴더로 복사
+            if (File.Exists(file_path))
+                File.Copy(file_path, temp_file_path);
+
+            // 스크립트를 실행하여, python과 연계하는 부분
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "cmd.exe";
+            psi.CreateNoWindow = true;
+            psi.Arguments = @"/C C:\C#\NP\client.bat";
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+
+            Process proc = Process.Start(psi);
+
+            // ML으로 파일을 보낸 결과를 받아옴
+            string cmd_result = proc.StandardOutput.ReadToEnd();
+            string[] cmd_result_split = cmd_result.Split();
+
+            string[] prediction_result = cmd_result_split[10].Split(',');
+            string cartegory = prediction_result[0];
+            string percent = prediction_result[1];
+
+            // 화면에 추론 결과를 표시
+            result_tbx.Text = cartegory + "(" + percent + ")";
+
+            return true;
+        }
+
+        bool isSearching = false;
+        private async void img_search_btn_Click(object sender, EventArgs e)
+        {
+            if (img_pbx.ImageLocation != null) // 사용자가 이미지를 먼저 드래그앤 드롭을 한 경우에만 진행
+            {
+                if (!isSearching)
+                {
+                    loading_pbx.Visible = true;
+                    isSearching = true;
+                    // 비동기식 처리를 통해 이미지 서칭 진행
+                    var result = Task.Run(() => Image_Searching());
+                    if (await result)
+                    {
+                        result.Dispose();
+                        loading_pbx.Visible = false;
+                        isSearching = false;
+                    }
+                }
+            }
+            else MessageBox.Show("이미지를 먼저 선택하여 주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+
+        private bool Image_View()
+        {
+            CheckForIllegalCrossThreadCalls = false; // 크로스 쓰레드 문제점 해결을 위한 예외처리
+            string image_path = @"C:\C#\NP\downloads";
+            if (!Directory.Exists(image_path)) Directory.CreateDirectory(image_path);
+             
+            while (true)
+            {
+                string[] files = Directory.GetFiles(image_path, "*.*", SearchOption.AllDirectories);
+
+                if (files.Count() >= 6)
+                {
+                    foreach (string file_path in files)
+                    {
+                        string file_ext = Path.GetExtension(file_path); // 확장자만 추출함 ex) .jpg or .jpeg 
+                        if (file_ext == ".jpeg" || file_ext == ".jpg") // 이미지 확장자인 경우에만 계속 진행
+                        {
+                            try
+                            {
+                                StreamReader sr = new StreamReader(file_path);
+                                Image img = Image.FromStream(sr.BaseStream);
+                                image_list.Images.Add(img);
+                                sr.Dispose();
+                            }
+                            catch {continue;}
+                        }
+                    }
+                    image_list.ImageSize = new Size(160, 160);
+                    image_lvw.LargeImageList = image_list;
+                    for (int j = 0; j < this.image_list.Images.Count; j++)
+                    {
+                        ListViewItem item = new ListViewItem();
+                        item.ImageIndex = j;
+                        this.image_lvw.Items.Add(item);
+                    }
+                    break;
+                }
+                Thread.Sleep(2000);
+            }
+            return true;
+        }
+
+
+        private bool Image_Download()
+        {
+            // 스크립트를 실행하여, python과 연계하는 부분
+            string command = @"""C:\Users\JUNG\Anaconda3\envs\tf\python.exe"" ""C:\C#\NP\image_download.py""";
+            string argv = result_tbx.Text.Substring(0, result_tbx.Text.IndexOf('('));
+
+            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            psi.RedirectStandardInput = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            Process cmd = new Process();
+            cmd.StartInfo = psi;
+            cmd.Start();
+            cmd.StandardInput.Write(@"cd ""C:\C#\NP""" + Environment.NewLine);
+            cmd.StandardInput.Write(@"" + command + " " + argv + Environment.NewLine);
+
+            return true;
+        }
+
+        bool isRunning = false;
+        private async void img_download_btn_Click(object sender, EventArgs e)
+        {
+            if (img_pbx.ImageLocation != null) // 사용자가 이미지를 먼저 드래그앤 드롭을 한 경우에만 진행
+            {
+                if (result_tbx.Text != "미동작 (0%)")
+                {
+                    if (!isRunning)
+                    {
+                        isRunning = true;
+                        loading_pbx.Visible = true;
+
+                        // 비동기식 처리를 통해 이미지 서칭 진행
+                        var download_result = Task.Run(() => Image_Download());
+                        if (await download_result)
+                        {
+                            download_result.Dispose();
+                        }
+
+                        var listing_result = Task.Run(() => Image_View());
+                        if (await listing_result)
+                        {
+                            listing_result.Dispose();
+                            isRunning = false;
+                            loading_pbx.Visible = false;
+                        }
+                    }
+                    else MessageBox.Show("이미지 다운로드가 이미 진행중입니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else MessageBox.Show("이미지 분석을 먼저 진행하여 주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else MessageBox.Show("이미지를 먼저 선택하여 주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void Main_MouseDown(object sender, MouseEventArgs e)
+        {
+            TogMove = 1;
+            MvalX = e.X;
+            MvalY = e.Y;
+        }
+
+        private void Main_MouseUp(object sender, MouseEventArgs e)
+        {
+            TogMove = 0;
+        }
+
+        private void Main_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (TogMove == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - MvalX, MousePosition.Y - MvalY);
+            }
+        }
+
+        private void close_btn_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
